83d3636fdd9a8cc5983e4c93d9d7816f6b346460
benjamin_morales <88735582+benjamin09111@users.noreply.github.com>
2023-05-11 08:50:57 -0400

    Add files via upload

---
+++ Ayudantía 2023 I Semestre/2. Clase a Clase/11-05-2023 herencia-ejercicios/1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+/*
+En un zoológico hay distintos animales. Se tienen tres
+zonas para que los animales vivan su vida libremente, son
+bastante grandes. En cada zona hay una cantidad de ese 
+animal, la cual puede ir aumentando en el tiempo. Cada
+zona posee el nombre del animal también. Las zonas son:
+aves, marinos y mamíferos terrestres. En cada zona
+se guarda algun tipo de animal, la cual debe preguntarse al
+usuario, igualmente que la cantidad que hay en cada uno.
+
+Además, la zona actuática posee kilos de comida, la de
+mamíferos la cantidad de huevos actualmente y la de 
+aves la cantidad de altura que posee la zona en km.
+
+Muestre las cantidades del animal y el nombre del animal de cada zona.
+Cree una clase Zoo que guarde Zonas.
+
+*/
+
+int main (){
+	int numero;
+	
+	
+	return 0;
+}
