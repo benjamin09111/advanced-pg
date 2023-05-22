6a0f6c3c0f00e9f56fafb91aeb634a2a335636e5
benjamin_morales <88735582+benjamin09111@users.noreply.github.com>
2023-05-22 16:39:38 -0400

    Add files via upload

---
+++ Ayudantía 2023 I Semestre/2. Clase a Clase/25-05-2023 vectores2/example-erase.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main (){
+	
+	vector<int> v;
+	
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4); //i=3 -> begin+i
+	v.push_back(5);
+	v.push_back(6);
+	
+	//erase: begin representa posicion cero
+	//uno o dos parámetros
+	v.erase(v.begin()+3);
+	
+	for(int i=0; i<v.size(); i++){
+		cout<<v[i]<<" ";
+	}
+	
+	
+	return 0;
+}
