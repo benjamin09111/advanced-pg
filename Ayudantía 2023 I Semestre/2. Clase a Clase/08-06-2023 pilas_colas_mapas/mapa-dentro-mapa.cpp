5a205d4b16970f7ef088d9c17bf1a71e3b0f707a
benjamin_morales <88735582+benjamin09111@users.noreply.github.com>
2023-06-08 09:47:18 -0400

    Add files via upload

---
+++ Ayudantía 2023 I Semestre/2. Clase a Clase/08-06-2023 pilas_colas_mapas/mapa-dentro-mapa.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+
+//hacer para cada mapa: crear y pares
+typedef pair<string, map<string,int> > par1;
+typedef map<string, map<string,int> > mapa1;
+
+typedef map<string,int>  par2;
+typedef map<string,int> mapa2;
+
+//iterador para cada mapa
+map<string, map<string,int> >::iterator i;
+map<string,int>::iterator j;
+
+int main(){
+	
+	
+}
